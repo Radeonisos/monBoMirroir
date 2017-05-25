75752b2e997ff891f43b6e189a1f9d3c3e51e707
David Fournier <fournierdavid49@yahoo.fr>
2017-05-25 12:01:30 +0200

    Inscription fonctionnelle

--- app/src/main/java/com/entreprise/davfou/monmirroir/login/RegisterActivity.java
+++ app/src/main/java/com/entreprise/davfou/monmirroir/login/RegisterActivity.java
@@ -2,21 +2,37 @@ package com.entreprise.davfou.monmirroir.login;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.transition.Explode;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.entreprise.davfou.monmirroir.R;
+import com.entreprise.davfou.monmirroir.rest.ApiInterface;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,6 +40,21 @@ public class RegisterActivity extends AppCompatActivity {
     FloatingActionButton fab;
     @BindView(R.id.cv_add)
     CardView cvAdd;
+    @BindView(R.id.bt_go)
+    Button bt_go;
+
+
+    @BindView(R.id.edit_nom)
+    EditText edit_nom;
+    @BindView(R.id.edit_prenom)
+    EditText edit_prenom;
+    @BindView(R.id.edit_age)
+    EditText edit_age;
+    @BindView(R.id.edit_ulr1)
+    EditText edit_ulr1;
+
+    ApiInterface apiInterface;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +69,23 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 animateRevealClose();
+            }
+        });
+
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        apiInterface = new Retrofit.Builder()
+                .baseUrl(ApiInterface.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(ApiInterface.class);
+
+        bt_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUer();
             }
         });
     }
@@ -121,4 +169,57 @@ public class RegisterActivity extends AppCompatActivity {
     public void onBackPressed() {
         animateRevealClose();
     }
+    private void addUer(){
+        final ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("En cours");
+        progress.setMessage("En cours d'envoi...");
+        progress.setCancelable(false);
+        progress.show();
+
+
+        Call<String> call = apiInterface.createUser(edit_nom.getText().toString(),edit_prenom.getText().toString(),Integer.parseInt(edit_age.getText().toString()),edit_ulr1.getText().toString(),true);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                progress.dismiss();
+
+              /*  System.out.println("response :"+response.message());
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("Réussi")
+                        .setMessage("Nouvel utilisateur correctement enregistré")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();*/
+                Explode explode = new Explode();
+                explode.setDuration(500);
+
+                getWindow().setExitTransition(explode);
+                getWindow().setEnterTransition(explode);
+                ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(RegisterActivity.this);
+                Intent i2 = new Intent(getApplicationContext(),LoginSuccessActivity.class);
+                startActivity(i2, oc2.toBundle());
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                progress.dismiss();
+                System.out.println("response :"+t.toString());
+
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("Erreur")
+                        .setMessage("Le nouvel utilisateur n'a pas pu être enregistrée, vérifier vôtre connexion.")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+
+
 }
+
